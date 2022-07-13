3ea6c7d1296f7e19d287cb89b51811a469e6aea3
Alejandro Vecchio <avecc004@fiu.edu>
2022-07-12 21:45:25 -0400

    Created model for Rating,Comment,User

---
+++ src/main/java/com/bookstore/GeekText/store/model/RatingComment.java
@@ -0,0 +1,81 @@
+package com.bookstore.GeekText.store.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+public class RatingComment {
+    @Entity
+    @Table (name = "rating_commenting")
+    public class RatingAndCommenting{
+        private int commentId;
+        private int userId;
+        private int isbn;
+        private float rating;
+        private String comment;
+
+        private Timestamp dateStamp;
+
+        public RatingAndCommenting(){
+        }
+
+        public RatingAndCommenting(int commentId, int userId, int isbn, float rating, String comment, Timestamp dateStamp){
+            this.commentId = commentId;
+            this.userId = userId;
+            this.isbn = isbn;
+            this.rating = rating;
+            this.comment = comment;
+            this.dateStamp = dateStamp;
+        }
+
+        //Setters and Getters
+
+        public int getCommentId() {
+            return commentId;
+        }
+
+        public void setCommentId(int commentId) {
+            this.commentId = commentId;
+        }
+
+        public int getUserId() {
+            return userId;
+        }
+
+        public void setUserId(int userId) {
+            this.userId = userId;
+        }
+
+        public int getIsbn() {
+            return isbn;
+        }
+
+        public void setIsbn(int isbn) {
+            this.isbn = isbn;
+        }
+
+        public float getRating() {
+            return rating;
+        }
+
+        public void setRating(float rating) {
+            this.rating = rating;
+        }
+
+        public String getComment() {
+            return comment;
+        }
+
+        public void setComment(String comment) {
+            this.comment = comment;
+        }
+
+        public Timestamp getDateStamp() {
+            return dateStamp;
+        }
+
+        public void setDateStamp(Timestamp dateStamp) {
+            this.dateStamp = dateStamp;
+        }
+    }
+}
