ca1f88d67313add6de1891933093c527697644c4
marielanicole <mtorr246@fiu.edu>
2022-07-15 16:35:49 -0400

    merged successfully

--- src/main/java/com/bookstore/GeekText/store/repository/MyRatingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.GeekText.store.repository;
-
-import com.bookstore.GeekText.store.model.RatingComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyRatingRepository extends JpaRepository<RatingComment, Integer> {
-}
