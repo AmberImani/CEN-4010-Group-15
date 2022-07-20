a722999a2b58478ab31b9c96595aa88733d97e03
Alejandro Vecchio <avecc004@fiu.edu>
2022-07-19 23:19:51 -0400

    Finished 1/4 feature bullet for rating and commenting

--- src/main/java/com/bookstore/GeekText/model/RatingComment.java
+++ src/main/java/com/bookstore/GeekText/model/RatingComment.java
@@ -1,39 +1,29 @@
 package com.bookstore.GeekText.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
-import java.io.Serializable;
 import java.math.BigInteger;
 import java.sql.Timestamp;
 @Entity
 @Table (name = "rating")
+@IdClass(RatingCommentId.class)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RatingComment{
     @Id
-    @Column(name = "user_id", nullable = false)
     private int user_id;
     @Id
-    @Column(name = "isbn", nullable = false)
     private BigInteger isbn;
-    @Column(name = "rating")
     private float rating;
-    @Column(name = "comment")
     private String comment;
     @Column(name = "date_stamp", nullable = false)
     private Timestamp date_stamp;
 
-    public RatingComment(){
-    }
-
-    public RatingComment(int user_id, BigInteger isbn, float rating, String comment, Timestamp date_stamp){
-
-        this.user_id = user_id;
-        this.isbn = isbn;
-        this.rating = rating;
-        this.comment = comment;
-        this.date_stamp = date_stamp;
-    }
-
     //Setters and Getters
-    @Id
     public int getUserId() {
         return user_id;
     }
@@ -41,7 +31,6 @@ public class RatingComment{
     public void setUserId(int userId) {
         this.user_id = userId;
     }
-    @Id
     public BigInteger getIsbn() {
         return isbn;
     }
